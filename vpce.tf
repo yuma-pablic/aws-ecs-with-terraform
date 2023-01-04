4f32e8654b71ca2b94b14e9f16cf8493529cdfad
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-04 15:37:52 +0900

    fix: sg

--- vpce.tf
+++ vpce.tf
@@ -27,18 +27,14 @@ resource "aws_vpc_endpoint" "sbcntr-vpce-ecr-s3" {
     vpc_id =  aws_vpc.sbcntrVpc.id
     service_name = "com.amazonaws.ap-northeast-1.s3"
     vpc_endpoint_type = "Gateway"
-    subnet_ids = [
-        aws_subnet.sbcntr-subnet-private-egress-1a.id, 
-        aws_subnet.sbcntr-subnet-private-egress-1c.id, 
-    ]
-    security_group_ids  = [aws_security_group.sbcntr-sg-vpce.id]
+    route_table_ids = [ aws_route_table.sbcntr-route-app.id ]
 }
 
 #Cloud watch logsにデータを送信する用
 resource "aws_vpc_endpoint" "sbcntr-vpce-ecr-logs" {
     vpc_id =  aws_vpc.sbcntrVpc.id
     service_name = "com.amazonaws.ap-northeast-1.logs"
-    vpc_endpoint_type = "Gateway"
+    vpc_endpoint_type = "Interface"
     subnet_ids = [
         aws_subnet.sbcntr-subnet-private-egress-1a.id, 
         aws_subnet.sbcntr-subnet-private-egress-1c.id, 
