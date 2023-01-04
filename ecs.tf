eede5e8ddab0dce13b31e509ae77c9e94ca2d6f6
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-05 00:46:40 +0900

    add: base ecs

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "self" {}
+
 # ECS Backend用クラスター
 resource "aws_ecs_cluster" "sbcntr-backend-cluster" {
   name               = "sbcntr-backend-cluster"
@@ -21,7 +23,7 @@ resource "aws_ecs_task_definition" "sbcntr-backend-def" {
   container_definitions = jsonencode([
     {
       name   = "app"
-      image  = ""
+      image  = "${data.aws_caller_identity.self.account_id}.dkr.ecr.ap-northeast-1.amazonaws.com/sbcntr-backend"
       cpu    = 256
       memory = 512
       portMappings = [
@@ -32,11 +34,93 @@ resource "aws_ecs_task_definition" "sbcntr-backend-def" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group : "/ecs/{task-definition-name}"
+          awslogs-group : "/ecs/sbcntr-backend-def"
           awslogs-region : "ap-northeast-1"
           awslogs-stream-prefix : "ecs"
         }
       }
     }
   ])
+}
+
+#ECS Backend用サービス
+resource "aws_ecs_service" "sbcntr-ecs-backend-service" {
+  name                               = "sbcntr-ecs-backend-service"
+  cluster                            = aws_ecs_cluster.sbcntr-backend-cluster
+  platform_version                   = "LATEST"
+  task_definition                    = aws_ecs_task_definition.sbcntr-backend-def.id
+  desired_count                      = 2
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  launch_type                        = "FARGATE"
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+  network_configuration {
+    subnets = [
+      aws_subnet.sbcntr-subnet-private-container-1a.id,
+      aws_subnet.sbcntr-subnet-private-container-1c.id,
+    ]
+    security_groups  = [aws_security_group.sbcntr-sg-internal.id]
+    assign_public_ip = false
+  }
+  health_check_grace_period_seconds = 120
+  load_balancer {
+    elb_name         = aws_alb.sbcntr-alb-internal.id
+    target_group_arn = aws_lb_target_group.sbcntr-tg-blue.id
+    container_name   = 80
+    container_port   = 80
+  }
+}
+
+#Code Deploy
+resource "aws_codedeploy_app" "AppECS-sbcntr-ecs-backend-cluster-sbcntr-ecs-backend-service" {
+  compute_platform = "ECS"
+  name             = "AppECS-sbcntr-ecs-backend-cluster-sbcntr-ecs-backend-service"
+}
+
+resource "aws_codedeploy_deployment_group" "Dpgsbcntr-ecs-backend-cluster-sbcntr-ecs-backend-service" {
+  app_name               = "AppECS-sbcntr-ecs-backend-cluster-sbcntr-ecs-backend-service"
+  deployment_group_name  = "Dpgsbcntr-ecs-backend-cluster-sbcntr-ecs-backend-service"
+  service_role_arn       = ""
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 10
+    }
+  }
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.sbcntr-backend-cluster
+    service_name = aws_ecs_service.sbcntr-ecs-backend-service
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.sbcntr-lisner-blue.id, aws_lb_listener.sbcntr-lisner-green.id]
+      }
+      target_group {
+        name = aws_lb_target_group.sbcntr-tg-blue.id
+      }
+      target_group {
+        name = aws_lb_target_group.sbcntr-tg-green.id
+      }
+    }
+  }
 }
