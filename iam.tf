ea037609e6dfa0e964e2182365ad57a6c15c232c
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-05 14:09:55 +0900

    add: policy

--- iam.tf
+++ iam.tf
@@ -1,5 +1,61 @@
+data "aws_caller_identity" "self" {}
+
+resource "aws_iam_policy" "sbcntr-AccessingECRRepositoryPolicy" {
+  name        = "sbcntr-AccessingECRRepositoryPolicy"
+  description = ""
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "ListImagesInRepository",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecr:ListImages"
+          ],
+          "Resource" : [
+            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-backend",
+            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-frontend"
+          ]
+        },
+        {
+          "Sid" : "GetAuthorizationToken",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecr:GetAuthorizationToken"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Sid" : "ManageRepositoryContents",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:GetRepositoryPolicy",
+            "ecr:DescribeRepositories",
+            "ecr:ListImages",
+            "ecr:DescribeImages",
+            "ecr:BatchGetImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:PutImage"
+          ],
+          "Resource" : [
+            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-backend",
+            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-frontend"
+          ]
+        }
+      ]
+    }
+  )
+
+}
+# Blue Green Deploymentを実行する際の権限
 resource "aws_iam_role" "ecs-codedeploy-role" {
-  name = "ecsCodeDeployRole"
+  name        = "ecsCodeDeployRole"
+  description = "Allows CodeDeploy to read S3 objects, invoke Lambda functions, publish to SNS topics, and update ECS services on your behalf."
   assume_role_policy = jsonencode(
     {
       "Version" : "2012-10-17",
@@ -8,13 +64,9 @@ resource "aws_iam_role" "ecs-codedeploy-role" {
           "Sid" : "",
           "Effect" : "Allow",
           "Principal" : {
-            "Service" : [
-              "codedeploy.amazonaws.com"
-            ]
+            "Service" : "codedeploy.amazonaws.com"
           },
-          "Action" : [
-            "sts:AssumeRole"
-          ]
+          "Action" : "sts:AssumeRole"
         }
       ]
     }
