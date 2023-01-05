3a6eab8b087f8c17a822ee3f47129172fe071fe7
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-05 13:44:15 +0900

    add: role

--- iam.tf
+++ iam.tf
@@ -1,17 +1,22 @@
-data "aws_iam_policy_document" "ecs-codedeploy-role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["codedeploy.amazonaws.com"]
-    }
-  }
-}
-
 resource "aws_iam_role" "ecs-codedeploy-role" {
-  assume_role_policy = jsonencode({
-
-  })
   name = "ecsCodeDeployRole"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "",
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : [
+              "codedeploy.amazonaws.com"
+            ]
+          },
+          "Action" : [
+            "sts:AssumeRole"
+          ]
+        }
+      ]
+    }
+  )
 }
