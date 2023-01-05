f39f06524f924fd578aea4da2f136c5476e96a45
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-05 22:03:01 +0900

    add: iam

--- iam.tf
+++ iam.tf
@@ -1,8 +1,7 @@
 data "aws_caller_identity" "self" {}
 
-resource "aws_iam_policy" "sbcntr-AccessingECRRepositoryPolicy" {
-  name        = "sbcntr-AccessingECRRepositoryPolicy"
-  description = "Policy to access ECR repo from Cloud9 instance"
+resource "aws_iam_policy" "sbcntr-accessing-ecr-repository-policy" {
+  name = "sbcntr-AccessingECRRepositoryPolicy"
   policy = jsonencode(
     {
       "Version" : "2012-10-17",
@@ -50,18 +49,11 @@ resource "aws_iam_policy" "sbcntr-AccessingECRRepositoryPolicy" {
       ]
     }
   )
-
 }
 
-resource "aws_iam_role" "sbcntr-cloud9-role" {
-  name               = "sbcntr-cloud9-role"
-  description        = ""
-  assume_role_policy = aws_iam_policy.sbcntr-AccessingECRRepositoryPolicy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "AAAA" {
-  role = aws_iam_role.sbcntr-cloud9-role.name
-  policy_arn = jsonencode(
+resource "aws_iam_policy" "sbcntr-administrater" {
+  name = "sbcntr-administrater"
+  policy = jsonencode(
     {
       "Version" : "2012-10-17",
       "Statement" : [
@@ -73,6 +65,34 @@ resource "aws_iam_role_policy_attachment" "AAAA" {
       ]
     }
   )
+}
+
+resource "aws_iam_role" "sbcntr-cloud9-role" {
+  name        = "sbcntr-cloud9-role"
+  description = "Allow EC2 instances to call AWS service on your behalf ."
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "iam-atachement-sbcntr-cloud9-role-admin" {
+  role       = aws_iam_role.sbcntr-cloud9-role.name
+  policy_arn = aws_iam_policy.sbcntr-administrater.arn
+}
+
+resource "aws_iam_role_policy_attachment" "iam-atachment-sbcntr-cloud9-role-ecr" {
+  role       = aws_iam_role.sbcntr-cloud9-role.name
+  policy_arn = aws_iam_policy.sbcntr-accessing-ecr-repository-policy.arn
 }
 # Blue Green Deploymentを実行する際の権限
 resource "aws_iam_role" "ecs-codedeploy-role" {
