6014155294d8dd5f9dd85a48a07513c6a2d636c2
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-14 15:06:16 +0900

    rm: role

--- iam.tf
+++ iam.tf
@@ -94,50 +94,6 @@ resource "aws_iam_role_policy_attachment" "iam-atachment-sbcntr-cloud9-role-ecr"
 }
 # Blue Green Deploymentを実行する際の権限
 
-resource "aws_iam_policy" "sbcntr-accessing-codedeploy-policy" {
-  name = "sbcntr-accessing-codedeploy-policy"
-  policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Action" : [
-            "ecs:DescribeServices",
-            "ecs:CreateTaskSet",
-            "ecs:UpdateServicePrimaryTaskSet",
-            "ecs:DeleteTaskSet",
-            "elasticloadbalancing:DescribeTargetGroups",
-            "elasticloadbalancing:DescribeListeners",
-            "elasticloadbalancing:ModifyListener",
-            "elasticloadbalancing:DescribeRules",
-            "elasticloadbalancing:ModifyRule",
-            "lambda:InvokeFunction",
-            "cloudwatch:DescribeAlarms",
-            "sns:Publish",
-            "s3:GetObject",
-            "s3:GetObjectVersion"
-          ],
-          "Resource" : "*",
-          "Effect" : "Allow"
-        },
-        {
-          "Action" : [
-            "iam:PassRole"
-          ],
-          "Effect" : "Allow",
-          "Resource" : "*",
-          "Condition" : {
-            "StringLike" : {
-              "iam:PassedToService" : [
-                "ecs-tasks.amazonaws.com"
-              ]
-            }
-          }
-        }
-      ]
-    }
-  )
-}
 resource "aws_iam_role" "ecs-codedeploy-role" {
   name               = "ecs-codedeploy-role"
   assume_role_policy = <<EOT
@@ -160,28 +116,4 @@ EOT
 resource "aws_iam_role_policy_attachment" "AWSCodeDeployRoleForECS" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
   role       = aws_iam_role.ecs-codedeploy-role.id
-}
-
-#cloudwatch logsにデータを送信をするための権限
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = { Service = "ecs-tasks.amazonaws.com" }
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-    "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
-  ]
-}
-
-resource "aws_iam_role_policy_attachment" "sbcntr-accessing-codedeploy-attachement" {
-  role       = aws_iam_role.ecs-codedeploy-role.name
-  policy_arn = aws_iam_policy.sbcntr-accessing-codedeploy-policy.arn
 }
