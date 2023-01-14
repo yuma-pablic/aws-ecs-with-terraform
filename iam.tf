749a743709bffde485d49f63d8c47f49e0cd3391
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-14 17:05:29 +0900

    add: secret

--- iam.tf
+++ iam.tf
@@ -113,7 +113,50 @@ resource "aws_iam_role" "ecs-codedeploy-role" {
 EOT
 }
 
-resource "aws_iam_role_policy_attachment" "AWSCodeDeployRoleForECS" {
+resource "aws_iam_role_policy_attachment" "aws-codedeploy-role-for-ecs-attachement" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
   role       = aws_iam_role.ecs-codedeploy-role.id
+}
+
+#Secret Maneger用
+resource "aws_iam_policy" "sbcntr-getting-secrets-policy" {
+  name = "sbcntr-GettingSecretsPolicy"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "GetSecretForECS",
+          "Effect" : "Allow",
+          "Action" : [
+            "secretsmanager:GetSecretValue"
+          ],
+          "Resource" : ["*"]
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role" "ecs_extension_assume_role" {
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2008-10-17",
+      "Statement" : [
+        {
+          "Sid" : "",
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "ecs-tasks.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "aws-ecs-codedeploy-role-atachement" {
+  policy_arn = aws_iam_policy.sbcntr-getting-secrets-policy.arn
+  role       = aws_ecs_task_definition.sbcntr-backend-def.execution_role_arn
 }
