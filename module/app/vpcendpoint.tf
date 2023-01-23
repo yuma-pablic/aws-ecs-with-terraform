1bb5b0ef27b3ffc594c866cba7b2257e0d7ed27f
yuma-pablic <yuumanagai0504pablic@gmail.com>
2023-01-23 20:28:35 +0900

    move: vpcendpoint

---
+++ module/app/vpcendpoint.tf
@@ -0,0 +1,44 @@
+# ECRからImageを取得する用
+resource "aws_vpc_endpoint" "sbcntr-vpce-ecr-api" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.api"
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
+  subnet_ids = [
+    aws_subnet.sbcntr-subnet-private-egress-1a.id,
+    aws_subnet.sbcntr-subnet-private-egress-1c.id,
+  ]
+  security_group_ids = [aws_security_group.sbcntr-sg-vpce.id]
+}
+
+resource "aws_vpc_endpoint" "sbcntr-vpce-ecr-dkr" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.dkr"
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
+  subnet_ids = [
+    aws_subnet.sbcntr-subnet-private-egress-1a.id,
+    aws_subnet.sbcntr-subnet-private-egress-1c.id,
+  ]
+  security_group_ids = [aws_security_group.sbcntr-sg-vpce.id]
+}
+
+resource "aws_vpc_endpoint" "sbcntr-vpce-s3" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.sbcntr-route-app.id]
+}
+
+#Cloud watch logsにデータを送信する用
+resource "aws_vpc_endpoint" "sbcntr-vpce-logs" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.ap-northeast-1.logs"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids = [
+    aws_subnet.sbcntr-subnet-private-egress-1a.id,
+    aws_subnet.sbcntr-subnet-private-egress-1c.id,
+  ]
+  security_group_ids = [aws_security_group.sbcntr-sg-vpce.id]
+}
