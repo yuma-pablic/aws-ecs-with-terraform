58e72c0cb7d6360682ceaf89e7a73ea5bea946b5
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-11 12:52:04 +0900

    fe

--- module/app/devops/codebuild.tf
+++ module/app/devops/codebuild.tf
@@ -57,3 +57,69 @@ resource "aws_iam_role_policy_attachment" "sbcntr-codebuild-attachement-role" {
   role       = aws_iam_role.sbcntr-codebuild-role.id
   policy_arn = aws_iam_policy.sbcntr-accessing-codecommit-policy.arn
 }
+resource "aws_iam_policy" "sbcntr-codebuild-policy" {
+  name        = "sbcntr-codebuild-policy"
+  description = "Policy used in trust relationship with CodeBuild"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Resource" : [
+            "arn:aws:logs:ap-northeast-1:${data.aws_caller_identity.self.account_id}:log-group:/aws/codebuild/sbcntr-codebuild",
+            "arn:aws:logs:ap-northeast-1:${data.aws_caller_identity.self.account_id}:log-group:/aws/codebuild/sbcntr-codebuild:*"
+          ],
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+        },
+        {
+          "Effect" : "Allow",
+          "Resource" : [
+            "arn:aws:s3:::${aws_s3_bucket.sbcntr-codepipline-bucket.id}*"
+          ],
+          "Action" : [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:GetBucketAcl",
+            "s3:GetBucketLocation"
+          ]
+        },
+        {
+          "Effect" : "Allow",
+          "Resource" : [
+            "arn:aws:codecommit:ap-northeast-1:${data.aws_caller_identity.self.account_id}:sbcntr-backend"
+          ],
+          "Action" : [
+            "codecommit:GitPull"
+          ]
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "codebuild:CreateReportGroup",
+            "codebuild:CreateReport",
+            "codebuild:UpdateReport",
+            "codebuild:BatchPutTestCases",
+            "codebuild:BatchPutCodeCoverages"
+          ],
+          "Resource" : [
+            "arn:aws:codebuild:ap-northeast-1:${data.aws_caller_identity.self.account_id}:report-group/sbcntr-codebuild-*"
+          ]
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "sbcntr-codebuild-attachement" {
+  role       = aws_iam_role.sbcntr-codebuild-role.id
+  policy_arn = aws_iam_policy.sbcntr-codebuild-policy.arn
+}
+
+
+
