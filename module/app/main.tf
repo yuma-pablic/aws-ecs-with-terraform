96c820896e1609e423fc393fa3d56bee8ceb7a7e
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-11 11:26:26 +0900

    fe

--- module/app/main.tf
+++ module/app/main.tf
@@ -710,63 +710,7 @@ resource "aws_iam_role_policy_attachment" "ecs-frontend-extension-role-attacheme
   role       = aws_iam_role.ecs-frontend-extension-role.id
 }
 
-resource "aws_codecommit_repository" "sbcntr-backend" {
-  repository_name = "sbcntr-backend"
-  description     = "Repository for sbcntr backend application"
-}
 
-resource "aws_iam_policy" "sbcntr-accessing-codecommit-policy" {
-  name = "sbcntr-AccessingCodeCommitPolicy"
-  policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Sid" : "ListImagesInRepository",
-          "Effect" : "Allow",
-          "Action" : [
-            "ecr:ListImages"
-          ],
-          "Resource" : [
-            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-backend",
-            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-frontend",
-            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-base"
-          ]
-        },
-        {
-          "Sid" : "GetAuthorizationToken",
-          "Effect" : "Allow",
-          "Action" : [
-            "ecr:GetAuthorizationToken"
-          ],
-          "Resource" : "*"
-        },
-        {
-          "Sid" : "ManageRepositoryContents",
-          "Effect" : "Allow",
-          "Action" : [
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:GetRepositoryPolicy",
-            "ecr:DescribeRepositories",
-            "ecr:ListImages",
-            "ecr:DescribeImages",
-            "ecr:BatchGetImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:PutImage"
-          ],
-          "Resource" : [
-            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-backend",
-            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-frontend",
-            "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-base"
-          ]
-        }
-      ]
-    }
-  )
-}
 
 resource "aws_iam_role" "sbcntr-codebuild-role" {
   name = "sbcntr-codebuild-role"
