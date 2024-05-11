92fff8623317f7a3db35b6512c57f1f0b394c9a6
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-11 10:50:09 +0900

    fe

---
+++ module/app/ecs.tf
@@ -0,0 +1,123 @@
+# ECS Backend用クラスター
+resource "aws_ecs_cluster" "sbcntr-backend-cluster" {
+  name               = "sbcntr-backend-cluster"
+  capacity_providers = ["FARGATE"]
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+  }
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+#ECS Backend用タスク定義
+resource "aws_ecs_task_definition" "sbcntr-backend-def" {
+  family                   = "sbcntr-backend-def"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = aws_iam_role.ecs-backend-extension-role.arn
+  task_role_arn            = aws_iam_role.sbcntr-ecsTaskRole.arn
+  container_definitions = jsonencode([
+    {
+      name               = "app"
+      image              = "${data.aws_caller_identity.self.account_id}.dkr.ecr.ap-northeast-1.amazonaws.com/sbcntr-backend:v1"
+      cpu                = 256
+      memory_reservation = 512
+      essential          = true
+      runtime_platform = {
+        operating_system_family = "LINUX"
+      }
+
+      portMappings = [
+        {
+          containerPort = 80
+        }
+      ]
+      # アプリのログはfirelensで出力
+      logConfiguration = {
+        logDriver = "awsfirelens"
+      }
+      }, {
+      essential         = true,
+      name              = "log_router"
+      image             = "${data.aws_caller_identity.self.account_id}.dkr.ecr.ap-northeast-1.amazonaws.com/sbcntr-base:log-router"
+      memoryReservation = 128,
+      cpu               = 64
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group : aws_cloudwatch_log_group.ecs-sbcntr-firelens-log-group.name,
+          awslogs-region : "ap-northeast-1",
+          awslogs-stream-prefix : "firelens"
+        }
+      },
+      firelensConfiguration = {
+        type = "fluentbit",
+        options = {
+          config-file-type  = "file",
+          config-file-value = "/fluent-bit/custom.conf"
+        }
+      },
+      environment = [
+        {
+          name : "APP_ID"
+          value : "backend-def"
+          }, {
+          name : "AWS_ACCOUNT_ID"
+          value : "${data.aws_caller_identity.self.account_id}"
+          }, {
+          name : "AWS_REGION"
+          value : "ap-northeast-1"
+          }, {
+          name : "LOG_BUCKET_NAME"
+          value : "sbcntr-${data.aws_caller_identity.self.account_id}"
+          }, {
+          name : "LOG_GROUP_NAME"
+          value : "/ecs/sbcntr-backend-def"
+        }
+      ],
+    }
+  ])
+}
+
+#ECS Backend用サービス
+resource "aws_ecs_service" "sbcntr-ecs-backend-service" {
+  depends_on                         = [aws_lb_listener.sbcntr-lisner-blue, aws_lb_listener.sbcntr-lisner-green]
+  name                               = "sbcntr-ecs-backend-service"
+  cluster                            = aws_ecs_cluster.sbcntr-backend-cluster.id
+  platform_version                   = "LATEST"
+  task_definition                    = aws_ecs_task_definition.sbcntr-backend-def.arn
+  desired_count                      = 2
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  launch_type                        = "FARGATE"
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+  network_configuration {
+    subnets = [
+      aws_subnet.sbcntr-subnet-private-container-1a.id,
+      aws_subnet.sbcntr-subnet-private-container-1c.id,
+    ]
+    security_groups  = [aws_security_group.sbcntr-sg-backend.id]
+    assign_public_ip = false
+  }
+  health_check_grace_period_seconds = 120
+  load_balancer {
+    target_group_arn = aws_lb_target_group.sbcntr-tg-blue.arn
+    container_name   = "app"
+    container_port   = 80
+  }
+  lifecycle {
+    ignore_changes = [
+      desired_count,
+      task_definition,
+      load_balancer,
+      network_configuration,
+      platform_version
+    ]
+  }
+}
