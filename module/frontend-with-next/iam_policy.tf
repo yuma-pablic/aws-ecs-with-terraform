820d0ad3210a7b1e7d87c7ac411763bfe223ca53
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-14 22:38:40 +0900

    fe

---
+++ module/frontend-with-next/iam_policy.tf
@@ -0,0 +1,12 @@
+data "aws_iam_policy_document" "ecs-frontend-extension-role-assume_role_policy" {
+  version = "2012-10-17"
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
