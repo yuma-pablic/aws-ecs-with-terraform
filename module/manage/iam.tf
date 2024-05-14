62049a83ce8fc12383e7be220590abff3e47d53a
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-14 21:19:58 +0900

    fe

--- module/manage/iam.tf
+++ module/manage/iam.tf
@@ -13,21 +13,20 @@ resource "aws_iam_policy" "sbcntr-administrater" {
   name   = "sbcntr-administrater"
   policy = data.aws_iam_policy_document.sbcntr-accessing-ecr-repository-policy-document.json
 }
-resource "aws_iam_role" "sbcntr-cloud9-role" {
-  name        = "sbcntr-cloud9-role"
-  description = "Allow EC2 instances to call AWS service on your behalf ."
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
+data "aws_iam_policy_document" "sbcntr-cloud9-role-policy-document" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:*"
     ]
-  })
+    resources = ["*"]
+  }
+}
+resource "aws_iam_role" "sbcntr-cloud9-role" {
+  name               = "sbcntr-cloud9-role"
+  description        = "Allow EC2 instances to call AWS service on your behalf ."
+  assume_role_policy = data.aws_iam_policy_document.sbcntr-cloud9-role-policy-document.json
 }
 resource "aws_iam_role_policy_attachment" "iam-atachement-sbcntr-cloud9-role-admin" {
   role       = aws_iam_role.sbcntr-cloud9-role.name
