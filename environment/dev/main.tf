3652ba510377c6e8c5e43179b5fb132b039b5203
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-17 21:41:03 +0900

    e

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,10 +1,5 @@
 #vpcの名前だけ入れる
 module "dev-network" {
   source = "../../module/network"
-  vpc_id = abs("dev")
-}
-
-module "dev-app" {
-  source = "../../module/app"
-  vpc_id = module.dev-vpc.vpd_id
+  vpc_id = module.dev-network.vpc_id
 }
