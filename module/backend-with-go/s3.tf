b13778bd61f7b2ea404c27a6a602c23fa46dc67b
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-23 00:29:38 +0900

    fe

--- module/backend-with-go/s3.tf
+++ module/backend-with-go/s3.tf
@@ -1,3 +1,3 @@
 resource "aws_s3_bucket" "sbcntr-account-id" {
-  bucket = "sbcntr-${data.aws_caller_identity.self.account_id}"
+  bucket = "${var.env}-${var.service}-${data.aws_caller_identity.self.account_id}"
 }
