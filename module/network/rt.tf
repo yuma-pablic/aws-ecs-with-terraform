8b971a6724cdbb95965eb25579d0fadcecb54afe
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-25 23:16:34 +0900

    fe

--- module/network/rt.tf
+++ module/network/rt.tf
@@ -1,20 +1,20 @@
-resource "aws_route_table" "route_app" {
+resource "aws_route_table" "api" {
   vpc_id = var.vpc_id
   tags = {
-    Name = "${var.env}-${var.service}-route-app"
+    Name = "${var.env}-${var.service}-rt-api"
   }
 }
 
 resource "aws_route_table_association" "private_1a" {
-  subnet_id      = aws_subnet.private_container_1a.id
-  route_table_id = aws_route_table.route_app.id
+  subnet_id      = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.api.id
 }
 
 resource "aws_route_table_association" "private_1c" {
-  subnet_id      = aws_subnet.private_container_1c.id
-  route_table_id = aws_route_table.route_app.id
+  subnet_id      = aws_subnet.private_1c.id
+  route_table_id = aws_route_table.api.id
 }
-resource "aws_route_table" "route_ingress" {
+resource "aws_route_table" "ingress" {
   vpc_id = var.vpc_id
   tags = {
     Name = "${var.env}-${var.service}-route-ingress"
@@ -22,34 +22,34 @@ resource "aws_route_table" "route_ingress" {
 }
 resource "aws_route_table_association" "public_ingress_1a" {
   subnet_id      = aws_subnet.public_ingress_1a.id
-  route_table_id = aws_route_table.route_ingress.id
+  route_table_id = aws_route_table.ingress.id
 }
 
 resource "aws_route_table_association" "public_ingress_1c" {
   subnet_id      = aws_subnet.public_ingress_1c.id
-  route_table_id = aws_route_table.route_ingress.id
+  route_table_id = aws_route_table.ingress.id
 }
 
 resource "aws_route" "public_route_table_connect_internet_gateway" {
-  route_table_id         = aws_route_table.route_ingress.id
+  route_table_id         = aws_route_table.ingress.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "public_management_1a" {
   subnet_id      = aws_subnet.public_management_1a.id
-  route_table_id = aws_route_table.route_ingress.id
+  route_table_id = aws_route_table.ingress.id
 }
 
 resource "aws_route_table_association" "public_management_1c" {
   subnet_id      = aws_subnet.public_management_1c.id
-  route_table_id = aws_route_table.route_ingress.id
+  route_table_id = aws_route_table.ingress.id
 }
 
 resource "aws_route_table" "db" {
   vpc_id = var.vpc_id
   tags = {
-    Name = "${var.env}-${var.service}-route-db"
+    Name = "${var.env}-${var.service}-rt-db"
   }
 }
 
--- module/network/subnet.tf
+++ module/network/subnet.tf
@@ -1,15 +1,15 @@
-resource "aws_subnet" "private_container_1a" {
+resource "aws_subnet" "private_1a" {
   vpc_id                  = var.vpc_id
   cidr_block              = "10.0.8.0/24"
   availability_zone       = "ap-northeast-1a"
   map_public_ip_on_launch = false
   tags = {
-    Name = "${var.env}-${var.service}-subnet-private-container-1a"
+    Name = "${var.env}-${var.service}-subnet-private-1a"
     Type = "Isolated"
   }
 }
 
-resource "aws_subnet" "private_container_1c" {
+resource "aws_subnet" "private_1c" {
   vpc_id                  = var.vpc_id
   cidr_block              = "10.0.9.0/24"
   availability_zone       = "ap-northeast-1c"
