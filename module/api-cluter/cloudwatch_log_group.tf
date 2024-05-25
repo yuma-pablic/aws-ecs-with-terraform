3eb40a9e89aeb2e8338995f0430e890ce9855528
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-25 22:19:56 +0900

    fe

--- module/api-cluter/cloudwatch_log_group.tf
+++ module/api-cluter/cloudwatch_log_group.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudwatch_log_group" "ecs_backend_def" {
-  name              = "/ecs/sbcntr-backend-def"
+  name              = "/ecs/${var.env}-${var.service}-api-def"
   retention_in_days = 30
 }
 resource "aws_cloudwatch_log_group" "ecs_firelens" {
-  name              = "/aws/ecs/sbcntr-firelens-container"
+  name              = "/aws/ecs/${var.env}-${var.service}-firelens-def"
   retention_in_days = 14
 }
