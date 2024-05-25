cf128addd6b4031634c1d3495cd949673f84ce35
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-25 23:45:06 +0900

    fe

--- module/network/outputs.tf
+++ module/network/outputs.tf
@@ -1,15 +1,15 @@
 output "vpd_id" {
-  value = aws_vpc.this.id
+  value = aws_vpc.main.id
 }
 output "vpce_sg" {
   value = aws_security_group.vpce.id
 }
 
 output "sg_api_id" {
-  value = aws_security_group.backend.id
+  value = aws_security_group.api.id
 }
 output "sg_web_id" {
-  value = aws_security_group.front_container.id
+  value = aws_security_group.web.id
 }
 
 output "sg_management_id" {
@@ -24,12 +24,12 @@ output "subnet_private_egress_1c_id" {
   value = aws_subnet.private_egress_1c.id
 }
 
-output "tg_blue_name" {
-  value = aws_lb_target_group.blue.name
+output "atg_blue_api_name" {
+  value = aws_lb_target_group.api_blue.name
 }
 
-output "tg_green_name" {
-  value = aws_lb_target_group.green.name
+output "tg_green_api_name" {
+  value = aws_lb_target_group.api_green.name
 }
 
 output "alb_web_arn" {
