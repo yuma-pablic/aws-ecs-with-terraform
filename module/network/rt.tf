560d12823ae49e6c6795e1ef0639180bf409f0e6
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-25 23:20:32 +0900

    fe

--- module/network/rt.tf
+++ module/network/rt.tf
@@ -30,7 +30,7 @@ resource "aws_route_table_association" "public_ingress_1c" {
   route_table_id = aws_route_table.ingress.id
 }
 
-resource "aws_route" "public_route_table_connect_internet_gateway" {
+resource "aws_route" "igw" {
   route_table_id         = aws_route_table.ingress.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
