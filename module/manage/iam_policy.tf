ce9f8cecbc3fca85c5a39d392be184dd723697d7
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-05-26 01:23:12 +0900

    fe

--- module/manage/iam_policy.tf
+++ module/manage/iam_policy.tf
@@ -18,3 +18,45 @@ data "aws_iam_policy_document" "full_access_cloud9" {
     resources = ["*"]
   }
 }
+
+data "aws_iam_policy_document" "ecr" {
+  version = "2012-10-17"
+  statement {
+    sid    = "ListImagesInRepository"
+    effect = "Allow"
+    actions = [
+      "ecr:ListImages"
+    ]
+    resources = [
+      "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-backend",
+    ]
+  }
+  statement {
+    sid    = "GetAuthorizationToken"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "ManageRepositoryContents"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage"
+    ]
+    resources = [
+      "arn:aws:ecr:ap-northeast-1:${data.aws_caller_identity.self.account_id}:repository/sbcntr-backend",
+    ]
+  }
+}
