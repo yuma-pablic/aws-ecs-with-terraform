250e10345d194a9213e9224efd4e8b6ce58d440a
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-07-03 00:33:45 +0900

    fe

--- envs/dev/main-api.tf
+++ envs/dev/main-api.tf
@@ -12,20 +12,20 @@
 #   aws_caller_identity = ""
 # }
 
-# module "db" {
-#   source                  = "../../modules/db"
-#   env                     = var.env
-#   service                 = var.service
-#   vpc_id                  = module.network.vpc_id
-#   sg_api_id               = module.network.sg_api_id
-#   subnet_group            = module.network.db_sb_group_id
-#   vpce_sg_id              = module.network.vpce_sg
-#   sb_private_egress_1a_id = module.network.sb_private_egress_1a_id
-#   sb_private_egress_1c_id = module.network.sb_private_egress_1c_id
-#   sg_manage_id            = module.network.sg_management_id
-#   sg_web_id               = module.network.sg_web_id
-#   sg_db_id                = module.network.sg_db_subnet
-# }
+module "db" {
+  source                  = "../../modules/db"
+  env                     = var.env
+  service                 = var.service
+  vpc_id                  = module.network.vpc_id
+  sg_api_id               = module.network.sg_api_id
+  subnet_group            = module.network.db_sb_group_id
+  vpce_sg_id              = module.network.vpce_sg
+  sb_private_egress_1a_id = module.network.sb_private_egress_1a_id
+  sb_private_egress_1c_id = module.network.sb_private_egress_1c_id
+  sg_manage_id            = module.network.sg_management_id
+  sg_web_id               = module.network.sg_web_id
+  sg_db_id                = module.network.sg_db_subnet
+}
 
 # module "devops" {
 #   source  = "../../modules/devops"
