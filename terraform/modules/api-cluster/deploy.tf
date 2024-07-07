4b4c5d384f8438c1fc8fa2471051f7aec1954639
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-07-07 13:12:17 +0900

    fe

--- terraform/modules/api-cluster/deploy.tf
+++ terraform/modules/api-cluster/deploy.tf
@@ -1,20 +1,20 @@
-resource "null_resource" "ecspresso" {
-  triggers = {
-    cluster            = aws_ecs_cluster.api.name,
-    execution_role_arn = aws_iam_role.ecs_task_execution.arn,
-  }
-  provisioner "local-exec" {
-    command     = "ecspresso deploy"
-    working_dir = var.ecspress_env_dir
-    environment = {
-      ECS_CLUSTER        = aws_ecs_cluster.api.name,
-      EXECUTION_ROLE_ARN = aws_iam_role.ecs_task_execution.arn
-    }
-  }
+# resource "null_resource" "ecspresso" {
+#   triggers = {
+#     cluster            = aws_ecs_cluster.api.name,
+#     execution_role_arn = aws_iam_role.ecs_task_execution.arn,
+#   }
+#   provisioner "local-exec" {
+#     command     = "ecspresso deploy"
+#     working_dir = var.ecspress_env_dir
+#     environment = {
+#       ECS_CLUSTER        = aws_ecs_cluster.api.name,
+#       EXECUTION_ROLE_ARN = aws_iam_role.ecs_task_execution.arn
+#     }
+#   }
 
-  provisioner "local-exec" {
-    command     = "ecspresso scale --tasks 0 && ecspresso delete --force"
-    working_dir = "../../../api/"
-    when        = destroy
-  }
-}
+#   provisioner "local-exec" {
+#     command     = "ecspresso scale --tasks 0 && ecspresso delete --force"
+#     working_dir = "../../../api/"
+#     when        = destroy
+#   }
+# }
