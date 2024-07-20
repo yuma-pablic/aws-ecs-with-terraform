d902a024017d5b3d8446531a0428b7590f9a9746
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-07-20 21:22:33 +0900

    fe

---
+++ terraform/modules/devops/iam_policy.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "oidc_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.self.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:yuma-pablic/aws-ecs-with-terraform:*"]
+    }
+  }
+}
