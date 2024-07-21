52d46fd843dca94a1e7feee99c1761de1cfac02b
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-07-21 17:06:55 +0900

    fe

--- terraform/modules/api-cluster/ecs.tf
+++ terraform/modules/api-cluster/ecs.tf
@@ -8,8 +8,8 @@ resource "aws_ecs_cluster" "api" {
 
 resource "aws_ecs_cluster_capacity_providers" "api" {
   cluster_name       = aws_ecs_cluster.api.name
-  capacity_providers = [var.env == "prod" ? "FARGATE" : "FARGATE_SPOT"]
+  capacity_providers = [var.env == "prod" || var.env == "stg" ? "FARGATE" : "FARGATE_SPOT"]
   default_capacity_provider_strategy {
-    capacity_provider = var.env == "prod" ? "FARGATE" : "FARGATE_SPOT"
+    capacity_provider = var.env == "prod" || var.env == "stg" ? "FARGATE" : "FARGATE_SPOT"
   }
 }
