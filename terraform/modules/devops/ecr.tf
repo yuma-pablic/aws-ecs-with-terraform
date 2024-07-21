556c2ab830cd1eeedae8777d8a4f513eb0361e09
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-07-21 17:31:43 +0900

    fe

--- terraform/modules/devops/ecr.tf
+++ terraform/modules/devops/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "api" {
   name                 = "sbcntr-api"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
@@ -9,7 +9,7 @@ resource "aws_ecr_repository" "api" {
 
 resource "aws_ecr_repository" "firelens" {
   name                 = "${var.service}firelens"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
