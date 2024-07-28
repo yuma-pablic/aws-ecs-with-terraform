33640f4bd4087b8848aa26b1d239cae28350a72a
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-07-28 19:56:41 +0900

    fe

---
+++ api/infrastructure/migrate/000001_create_users_table.up.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE users;
