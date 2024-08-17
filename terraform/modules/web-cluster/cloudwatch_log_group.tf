3c81a49f4f643cfedea2c31cc4b4cc6974c5f3ff
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-08-18 02:00:12 +0900

    fe

--- terraform/modules/web-cluster/cloudwatch_log_group.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "ecs_web_def" {
-  name              = "${var.env}-${var.service}-ecs-web-def"
-  retention_in_days = 30
-}
--- terraform/modules/web-cluster/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "service" {
-  type = string
-}
-variable "env" {
-  type = string
-}
