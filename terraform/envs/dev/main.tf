1c6932cd0016cdd0dcb814d4aba4c4a92d7f6903
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-08-19 21:53:32 +0900

    fe

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,22 +1,25 @@
-# module "network" {
-#   source  = "../../modules/network"
-#   service = var.service
-#   env     = var.env
-# }
+module "network" {
+  source  = "../../modules/network"
+  service = var.service
+  env     = var.env
+}
+data "aws_alb_listener" "network" {
+  arn = module.network.listener_blue.arn
+}
 
-# module "api-cluster" {
-#   source           = "../../modules/api-cluster"
-#   service          = var.service
-#   env              = var.env
-#   tg_blue          = module.network.tg_blue_api_name
-#   tg_green         = module.network.tg_green_api_name
-#   listener_blue    = module.network.listener_blue.arn
-#   listener_green   = module.network.listener_green.arn
-#   sb_private_1a    = module.network.sb_private_egress_1a_id
-#   sb_private_1c    = module.network.sb_private_egress_1c_id
-#   sg_api           = module.network.sg_api_id
-#   ecspress_env_dir = var.ecspresso_env_dir
-# }
+module "api-cluster" {
+  source           = "../../modules/api-cluster"
+  service          = var.service
+  env              = var.env
+  tg_blue          = module.network.tg_blue_api_name
+  tg_green         = module.network.tg_green_api_name
+  listener_blue    = module.network.listener_blue.arn
+  listener_green   = module.network.listener_green.arn
+  sb_private_1a    = module.network.sb_private_egress_1a_id
+  sb_private_1c    = module.network.sb_private_egress_1c_id
+  sg_api           = module.network.sg_api_id
+  ecspress_env_dir = var.ecspresso_env_dir
+}
 
 
 # module "api" {
@@ -30,7 +33,7 @@
 #   source                  = "../../modules/db"
 #   env                     = var.env
 #   service                 = var.service
-#   vpc_id                  = module.network.vpc_id
+#   vpc_id                  = data.aws_vpc.network.id
 #   sg_api_id               = module.network.sg_api_id
 #   subnet_group            = module.network.db_sb_group_id
 #   vpce_sg_id              = module.network.vpce_sg
